bf37bed3f61dc78a15d43795c71da61c28597be6
Rifa Jafri <rifajafri@Rifas-MacBook-Pro.local>
2018-03-08 15:18:21 -0800

    added css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,27 +8,49 @@ var config = {
   };
   firebase.initializeApp(config);
 
+  
   var database = firebase.database()
-
-$("#addTrain").on("click", function(event) {
+  $("#addTrain").on("click", function(event) {
     event.preventDefault()
 
     var trainName = $("#train-name").val().trim()
     var trainDes = $("#train-dest").val().trim()
     var trainFreq = $("#train-frequency").val().trim()
     var trainArrival = $("#train-arrival").val().trim()
-    var trainMinAway = $("#min-away").val().trim()
+    // var trainMinAway = $("#min-away").val().trim()
     console.log(response)
 
-    var newTrain = {
+    database.push({
         name: trainName,
         destination: trainDes,
         frequency: trainFreq,
         arrival: trainArrival,
-        minutes: trainMinAway
-    }
 
+    })
 
-
+    // var newTrain = {
+    //     minutes: trainMinAway
+    // }
 })
+
+database.ref().on("child_added", function(snap) {
+    var trainFrequency = snap.val().frequency
+    var dateConvert = moment(snap.val().arrival, 'hh:mm').subtract(1, "years")
+    var trainTime = moment(dateConvert).format("HH:mm")
+    var now = moment()
+    var firstConvert = moment(trainTime, 'hh:mm').subtract(1, "years")
+    var timeDifference = moment().diff(moment(firstConvert), "minutes")
+    var remainingTime = timeDifference % trainFrequency
+    var tMinus = trainFrequency - remainingTime
+    var nextTrain = moment().add(tMinus, "minutes").format("HH:mm")
+
+    
+
+  //append DOM
+  $("#trainSchedule").append("<tr><td>" + snap.val().name + "</td><td>" +
+  snap.val().destination + "</td><td>" + snap.val().frequency +
+  "</td><td>" + trainTime + "</td><td>" + tMinus + "</td></tr>")
+  },function(errorObject) {
+  })
+// })
     
